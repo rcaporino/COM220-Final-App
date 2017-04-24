7e9957000e678d4b53eb4d7aedabab7eec69f452
dwinter56 <dwinter@student.sjcny.edu>
2017-04-24 18:46:22 -0400

    added webview
    needs button

--- app/src/main/java/com/example/android/com220drunkapp/MainActivity.java
+++ app/src/main/java/com/example/android/com220drunkapp/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.android.com220drunkapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+//hangover remedies webview
+    public void seeRemedies(View view) {
+        setContentView(R.layout.activity_hangover_webview);
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl("http://content.time.com/time/specials/packages/completelist/0,29569,2039990,00.html");
+    }
+
+
+
+
+
+
 }
