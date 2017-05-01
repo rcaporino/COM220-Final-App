05e5edfab652674337577901eb0e31475562da95
John Baker <jbaker@student.sjcny.edu>
2017-05-01 19:20:20 -0400

    Linked up: Last call Eating Cheers E contact Find Friends Story

--- app/src/main/java/com/example/android/com220finalapp/EmergencyContact.java
+++ app/src/main/java/com/example/android/com220finalapp/EmergencyContact.java
@@ -1,8 +1,18 @@
+
 package com.example.android.com220finalapp;
 
-/**
- * Created by Matt on 4/10/2017.
- */
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.widget.EditText;
+        import android.widget.RadioGroup;
 
-public class EmergencyContact {
+        import java.util.Date;
+
+public class EmergencyContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eating);
+    }
 }
