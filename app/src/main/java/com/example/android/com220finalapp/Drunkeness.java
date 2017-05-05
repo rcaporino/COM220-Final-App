bb4ab528b8f6f90a618c6bb6ca470b4526f7c62b
rcaporino <rcaporino@gmail.com>
2017-05-05 18:44:58 -0400

    Fixed some small bugs in the math

--- app/src/main/java/com/example/android/com220finalapp/Drunkeness.java
+++ app/src/main/java/com/example/android/com220finalapp/Drunkeness.java
@@ -7,10 +7,12 @@ package com.example.android.com220finalapp;
         import service_and_storage.Drink;
         import service_and_storage.User;
 
-public class Drunkeness extends AppCompatActivity {
+public class Drunkeness extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drunkenessui);
     }
@@ -20,25 +22,30 @@ public class Drunkeness extends AppCompatActivity {
     /**
      * Calculates and sets blood alcohol level for a user
      * @param user user object
-     * @param drinks linkedlist of drinks
+     * @param allDrinks linkedlist of all drinks
      */
     /**
      * I think it would also make more sense to get the current time as a
      * parameter but I said I would be getting the current time in class
      */
-    public void setBloodAlcohol(User user, List<Drink> drinks)
+    public void setBloodAlcohol(User user, List<Drink> allDrinks)
     {
 
-        double bloodAlcohol = 0;
-        double gramsOfAlc = 0;
+        double bloodAlcohol = 0.0;
+        double gramsOfAlc = 0.0;
         double oz;
         double proof;
         double timeDiff;
         double gc = 0;
         long firstDrinkTime = 0;
         double weight = user.getWeight();
+        double timeCheck = 0.0;
+        List<Drink> validDrinks = new LinkedList<>();
         User.Gender gender = user.getGender();
 
+        java.util.Date today = new java.util.Date();
+        java.sql.Timestamp time1 = new java.sql.Timestamp(today.getTime());
+        long timeNow = time1.getTime();
 
         if(gender == User.Gender.Male)
         {
@@ -50,16 +57,33 @@ public class Drunkeness extends AppCompatActivity {
             gc = 0.0;
         }
 
-        //Iterate over drink list to find the first drinks time drank
-        for (Drink drink : drinks)
-        {   //if it hasnt been set yet, set it to the first elements time in the list
+        //Itereate over allDrinks list to only take drinks that were drunk within
+        //the past 12 hours;
+        for(Drink drink: allDrinks)
+        {
+            // set timeCheck to the current time - the current elements time drank to
+            // a number of hours
+            timeCheck = (timeNow - drink.getTimeDrank()) / (60.0 * 60.0 * 1000.0);
+
+            // If timeCheck is less than 12 hours then add the current element to
+            // the validDrink list
+            if(timeCheck < 12.0)
+            {
+                validDrinks.add(drink);
+            }
+        }
+
+        // Iterate over validDrinks list to find the first drinks time drank
+        for (Drink drink : validDrinks)
+        {   // If it hasnt been set yet, set it to the first elements time in the list
+
             if (firstDrinkTime == 0)
             {
                 firstDrinkTime = drink.getTimeDrank();
 
             }
             else
-            {   //check if the current element's time drank is less than the first time drank,
+            {   // check if the current element's time drank is less than the first time drank,
                 // set firstDrankTime to current elements time.
                 if (firstDrinkTime > drink.getTimeDrank())
                 {
@@ -69,15 +93,12 @@ public class Drunkeness extends AppCompatActivity {
             }
         }
 
-        java.util.Date today = new java.util.Date();
-        java.sql.Timestamp time1 = new java.sql.Timestamp(today.getTime());
-        long timeNow = time1.getTime();
 
         //Calcuate elapsed time
         timeDiff = timeNow - firstDrinkTime;
 
-        //Iterate over drink list, calculate BAC for each drink
-        for(Drink drink : drinks)
+        //Iterate over validDrinks list, calculate BAC for each drink
+        for(Drink drink : validDrinks)
         {
 
             oz = drink.getSizeInOz();
@@ -87,7 +108,7 @@ public class Drunkeness extends AppCompatActivity {
             timeDiff = timeDiff / (60.0 * 60.0 * 1000.0);
 
             gramsOfAlc = oz * (proof / 100.0) / 2.0;
-            bloodAlcohol += (gramsOfAlc * 5.14) / (weight * gc) - (.015 * timeDiff);
+            bloodAlcohol += (gramsOfAlc * 5.14) / (weight * gc) - (.015 * timeDiff) * 1;
             /**
              * Widmark formula % BAC = (A x 5.14 / W x r) – .015 x H
              * A = grams of alcohol
--- app/src/test/java/com/example/android/com220finalapp/ExampleUnitTest.java
+++ app/src/test/java/com/example/android/com220finalapp/ExampleUnitTest.java
@@ -25,16 +25,18 @@ public class ExampleUnitTest
         Drunkeness test1 = new Drunkeness();
 
         java.util.Date today = new java.util.Date();
-        java.sql.Timestamp time1 = java.sql.Timestamp.valueOf("2017-05-05 21:00:10");
+        java.sql.Timestamp time1 = java.sql.Timestamp.valueOf("2017-05-05 14:00:10");
         long timeNow = time1.getTime();
 
-        java.sql.Timestamp ts1 = java.sql.Timestamp.valueOf("2017-05-05 10:37:10");
-        java.sql.Timestamp ts2 = java.sql.Timestamp.valueOf("2017-05-05 11:00:10");
-        java.sql.Timestamp ts3 = java.sql.Timestamp.valueOf("2017-05-05 11:10:10");
+        java.sql.Timestamp ts1 = java.sql.Timestamp.valueOf("2017-05-04 13:03:10");
+        java.sql.Timestamp ts2 = java.sql.Timestamp.valueOf("2017-05-05 13:43:10");
+        java.sql.Timestamp ts3 = java.sql.Timestamp.valueOf("2017-05-05 14:13:10");
+        java.sql.Timestamp ts4 = java.sql.Timestamp.valueOf("2017-05-05 14:43:09");
 
         long time10 = ts1.getTime();
         long time2 = ts2.getTime();
         long time3 = ts3.getTime();
+        long time4 = ts4.getTime();
 
         User user = new User(72, 200, User.Gender.Male);
         List<Drink> drinkList = new LinkedList<>();
@@ -42,9 +44,11 @@ public class ExampleUnitTest
         Drink drink1 = new Drink("troll1", 1.5, 80.0, time10);
         Drink drink2 = new Drink("troll2", 1.5, 80.0, time2);
         Drink drink3 = new Drink("troll3", 1.5, 80.0, time3);
+        Drink drink4 = new Drink("troll4", 1.5, 80.0, time4);
         drinkList.add(drink1);
         drinkList.add(drink2);
         drinkList.add(drink3);
+        drinkList.add(drink4);
         //call function with test data
         test1.setBloodAlcohol(user, drinkList);
         //do assertions
