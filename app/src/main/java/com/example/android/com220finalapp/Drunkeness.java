d40d80ac2f8c5e884a597e58cfcef10a09d3ebdb
rcaporino <rcaporino@gmail.com>
2017-05-05 11:45:43 -0400

    Math is basically done, I still need to do something with the meals I guess but food doesnt actually affect your BAC.

--- app/src/main/java/com/example/android/com220finalapp/Drunkeness.java
+++ app/src/main/java/com/example/android/com220finalapp/Drunkeness.java
@@ -1,28 +1,11 @@
 package com.example.android.com220finalapp;
 
-        import android.content.Intent;
         import android.os.Bundle;
         import android.support.v7.app.AppCompatActivity;
-        import android.view.View;
-        import android.widget.AdapterView;
-        import android.widget.ArrayAdapter;
-        import android.widget.CheckBox;
-        import android.widget.EditText;
-        import android.widget.RadioButton;
-        import android.widget.TextView;
-        import android.widget.Toast;
-
-
-        import org.w3c.dom.Text;
-
-        import java.sql.Timestamp;
-        import java.text.SimpleDateFormat;
         import java.util.*;
-
         import service_and_storage.DataCollection;
         import service_and_storage.Drink;
         import service_and_storage.User;
-
 
 public class Drunkeness extends AppCompatActivity {
 
@@ -32,159 +15,92 @@ public class Drunkeness extends AppCompatActivity {
         setContentView(R.layout.drunkenessui);
     }
 
-//    int numDrinks = 0;
-//    double highestBAC = 0.0;
-//    double currentBAC = 0.0;
-//    double newBAC = 0;
-//    double r = 0;
-//    double n = 0.789;
-//    double poundsToGrams = 453.59237;
-//    double time = 0;
-//    double weightInGrams = 0;
-//    double gc = 0;
-//    double rawNum = 0;
-
 
     DataCollection data = new DataCollection();
 
-
-
-    public void start(View view)
-
+    /**
+     * Calculates and sets blood alcohol level for a user
+     * @param user user object
+     * @param drinks linkedlist of drinks
+     */
+    /**
+     * I think it would also make more sense to get the current time as a
+     * parameter but I said I would be getting the current time in class
+     */
+    public void setBloodAlcohol(User user, List<Drink> drinks)
     {
 
-//
-//        EditText weight = (EditText) findViewById(R.id.weight);
-//        String weightValue = weight.getText().toString();
-//        double w = Double.parseDouble(weightValue);
-//
-//        EditText percent = (EditText) findViewById(R.id.alcohol_percent);
-//        String percentValue = percent.getText().toString();
-//        double ap = Double.parseDouble(percentValue);
-//
-//        EditText ounces = (EditText) findViewById(R.id.ounces);
-//        String ouncesValue = ounces.getText().toString();
-//        double oz = Double.parseDouble(ouncesValue);
-//
-//        EditText numDrinks = (EditText) findViewById(R.id.num_d);
-//        String numDrinksValue = numDrinks.getText().toString();
-//        double nd = Double.parseDouble(numDrinksValue);
-//
-//        EditText hours = (EditText) findViewById(R.id.hours);
-//        String hoursValue = hours.getText().toString();
-//        double h = Double.parseDouble(hoursValue);
+        double bloodAlcohol = 0;
+        double gramsOfAlc = 0;
+        double oz;
+        double proof;
+        double timeDiff;
+        double gc = 0;
+        long firstDrinkTime = 0;
+        double weight = user.getWeight();
+        User.Gender gender = user.getGender();
 
 
-//        radioCheck();
-//        calcBAC(w, ap, oz, nd, h);
-//        setTextViews();;
+        if(gender == User.Gender.Male)
+        {
+            gc = 0.73;
+        }else if(gender == User.Gender.Female)
+        {
+            gc = 0.66;
+        }else{
+            gc = 0.0;
+        }
 
-    }
-//
-//    public void radioCheck() {
-//        RadioButton male = (RadioButton) findViewById(R.id.male);
-//        RadioButton female = (RadioButton) findViewById(R.id.female);
-//        boolean m = male.isChecked();
-//        boolean f = female.isChecked();
-//        if (m == true) {
-//            gc = 0.73;
-//        } else if (f == true) {
-//            gc = 0.66;
-//        }
-//    }
+        //Iterate over drink list to find the first drinks time drank
+        for (Drink drink : drinks)
+        {   //if it hasnt been set yet, set it to the first elements time in the list
+            if (firstDrinkTime == 0)
+            {
+                firstDrinkTime = drink.getTimeDrank();
 
-//    public void calcBAC(double weight, double alcPercent, double alcOunces, double numDrinks, double hours)
-//    {
-//
-//
-//                        //1             ounces        percent = proof/2
-//        gramsOfAlc = (numDrinks) * (alcOunces) * (alcPercent/100);
-//        currentBAC = ((gramsOfAlc * 5.14) / (weight * gc)) - (.015 * hours);
-//
-//        if(currentBAC > highestBAC)
-//            highestBAC = currentBAC;
-//
-//
-//    }
+            }
+            else
+            {   //check if the current element's time drank is less than the first time drank,
+                // set firstDrankTime to current elements time.
+                if (firstDrinkTime > drink.getTimeDrank())
+                {
+                    firstDrinkTime = drink.getTimeDrank();
+                }
 
-    public void setBloodAlcohol()
-    {
-
-//        java.util.Date today = new java.util.Date();
-//        java.sql.Timestamp time1 = new java.sql.Timestamp(today.getTime());
-//
-//        java.sql.Timestamp ts2 = java.sql.Timestamp.valueOf("2017-05-03 16:15:10");
-//
-//        long timeNow = time1.getTime();
-//        long timeTest = ts2.getTime();
-//
-//        long diff = timeNow - timeTest;
-//        long diffhours = diff / (60 * 60 * 1000);
-//
-//        System.out.println(diffhours);
+            }
+        }
 
         java.util.Date today = new java.util.Date();
         java.sql.Timestamp time1 = new java.sql.Timestamp(today.getTime());
         long timeNow = time1.getTime();
-        System.out.println(timeNow);
 
-        User user = new User();
-        List<Drink> listOfDrinks;
-        listOfDrinks = data.getDrinksConsumed();
+        //Calcuate elapsed time
+        timeDiff = timeNow - firstDrinkTime;
 
-        double bloodAlcohol = 0;
-        double gramsOfAlc = 0;
-        double weight = 0;
-        double gc = 0;
-        long diff = 0;
-        long diffhours = 0;
-
-
-        double [] oz = new double[listOfDrinks.size()];
-        double [] proof = new double [listOfDrinks.size()];
-        long [] timeDrank = new long[listOfDrinks.size()];
-
-
-
-        weight = user.getWeight();
-
-        User.Gender gender = user.getGender();
-        if (gender == User.Gender.Male)
+        //Iterate over drink list, calculate BAC for each drink
+        for(Drink drink : drinks)
         {
-            gc = 0.73;
-        }else if (gender == User.Gender.Female) {
-            gc = 0.66;
-        }
 
-        for(int i = 0; i < listOfDrinks.size(); i++)
-        {
-            oz[i] = listOfDrinks.get(i).getSizeInOz();
-            proof[i] = listOfDrinks.get(i).getProof();
-            timeDrank[i] = listOfDrinks.get(i).getTimeDrank();
+            oz = drink.getSizeInOz();
+            proof = drink.getProof();
 
-            diff = timeNow - timeDrank[i];
-            diffhours =  diff / (60 * 60 * 1000);
+            //Converting milliseconds to hours
+            timeDiff = timeDiff / (60.0 * 60.0 * 1000.0);
 
-            gramsOfAlc = (oz[i]) * ((proof[i]/100))/(2);
-            bloodAlcohol += (gramsOfAlc * 5.14) / (weight * gc) - (.015 * diffhours);
+            gramsOfAlc = oz * (proof / 100.0) / 2.0;
+            bloodAlcohol += (gramsOfAlc * 5.14) / (weight * gc) - (.015 * timeDiff);
+            /**
+             * Widmark formula % BAC = (A x 5.14 / W x r) – .015 x H
+             * A = grams of alcohol
+             * W = Weight in lbs
+             * r = gender constant
+             * H = hours passed
+             * http://www.teamdui.com/bac-widmarks-formula/
+             */
         }
 
         user.setIntoxLevel(bloodAlcohol);
 
-
     }
 
-//    public void setTextViews()
-//    {
-//        TextView drinkNumber = (TextView) findViewById(R.id.num_drink);
-//        drinkNumber.setText("" + numDrinks);
-//
-//        TextView timeSinceLast = (TextView) findViewById(R.id.timer);
-//
-//        TextView record = (TextView) findViewById(R.id.highest_bac);
-//        record.setText("" + highestBAC);
-//
-//        TextView current = (TextView) findViewById(R.id.current_bac);
-//        current.setText("" + currentBAC);
-//    }
 }
--- app/src/test/java/com/example/android/com220finalapp/ExampleUnitTest.java
+++ app/src/test/java/com/example/android/com220finalapp/ExampleUnitTest.java
@@ -2,16 +2,51 @@ package com.example.android.com220finalapp;
 
 import org.junit.Test;
 
+import java.util.LinkedList;
+import java.util.List;
+import com.example.android.com220finalapp.Drunkeness;
+import service_and_storage.Drink;
+import service_and_storage.User;
+
 import static org.junit.Assert.*;
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class ExampleUnitTest
+{
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void setBloodAlcoholTest_success()
+    {
+        Drunkeness test1 = new Drunkeness();
+
+        java.util.Date today = new java.util.Date();
+        java.sql.Timestamp time1 = java.sql.Timestamp.valueOf("2017-05-05 21:00:10");
+        long timeNow = time1.getTime();
+
+        java.sql.Timestamp ts1 = java.sql.Timestamp.valueOf("2017-05-05 10:37:10");
+        java.sql.Timestamp ts2 = java.sql.Timestamp.valueOf("2017-05-05 11:00:10");
+        java.sql.Timestamp ts3 = java.sql.Timestamp.valueOf("2017-05-05 11:10:10");
+
+        long time10 = ts1.getTime();
+        long time2 = ts2.getTime();
+        long time3 = ts3.getTime();
+
+        User user = new User(72, 200, User.Gender.Male);
+        List<Drink> drinkList = new LinkedList<>();
+        // Add some drinks to drinkList
+        Drink drink1 = new Drink("troll1", 1.5, 80.0, time10);
+        Drink drink2 = new Drink("troll2", 1.5, 80.0, time2);
+        Drink drink3 = new Drink("troll3", 1.5, 80.0, time3);
+        drinkList.add(drink1);
+        drinkList.add(drink2);
+        drinkList.add(drink3);
+        //call function with test data
+        test1.setBloodAlcohol(user, drinkList);
+        //do assertions
+        assertEquals(.048, user.getIntoxLevel(), .001);
     }
 }
