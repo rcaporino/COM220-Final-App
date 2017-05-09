45c2d42704db5db13a052a05cd5d30ace926da8a
John Baker <jbaker@student.sjcny.edu>
2017-05-08 20:37:14 -0400

    Added permission requests on app open

--- app/src/main/java/com/example/android/com220finalapp/MainActivity.java
+++ app/src/main/java/com/example/android/com220finalapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.android.com220finalapp;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ViewSwitcher;
@@ -16,6 +21,7 @@ import service_and_storage.User;
 public class MainActivity extends AppCompatActivity {
     private ViewSwitcher simpleViewSwitcher;
     ImageButton btnChange;
+    public boolean hasStorageReadPerms = false, hasStorageWritePerms=false;
 
     //Service service = Service.getInstance();
     //User user = service.getUser();
@@ -31,9 +37,75 @@ public class MainActivity extends AppCompatActivity {
 
         btnChange = (ImageButton) findViewById(R.id.refresh);
         simpleViewSwitcher = (ViewSwitcher) findViewById(R.id.drink);
+        reqPermissions(1);
+        reqPermissions(2);
     }
 
+    public void reqPermissions(int i) {
+        switch (i) {
+            case 1: {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    Log.i("BakerReq", "Requesting Write Permissions");
+                    ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            1);
+                } else {
+                    Log.i("BakerReq", "Has Write Permissions");
+                    this.hasStorageWritePerms = true;
+                }
+            }
+            case 2:{
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    Log.i("BakerReq", "Requesting Read Permissions");
+                    ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},
+                            2);
+                } else {
+                    Log.i("BakerReq", "Has Read Permissions");
+                    this.hasStorageReadPerms = true;
+                }
+            }
+        }
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    hasStorageWritePerms = true;
+                    reqPermissions(2);
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+                    reqPermissions(1);
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+            case 2:{
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    hasStorageReadPerms = true;
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+                    reqPermissions(2);
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
     public void changeBtn(View view)
     {
         //Intent intent = new Intent(MainActivity.this, ChangeActivity.class);
