3f759d588f88a3b93311767208b8516b8490227b
rcaporino <rcaporino@gmail.com>
2017-05-09 21:59:09 -0400

    Made the class to actually add Drinks and the xml to go with it.

--- app/src/main/java/com/example/android/com220finalapp/MyDrink.java
+++ app/src/main/java/com/example/android/com220finalapp/MyDrink.java
@@ -1,13 +1,86 @@
 package com.example.android.com220finalapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.util.List;
+import service_and_storage.Drink;
+import service_and_storage.Service;
 
-public class MyDrink extends AppCompatActivity {
+public class MyDrink extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_drink);
+
+        double BAC = Service.getInstance().getUserIntoxLevel();
+
+        TextView displayBAC = (TextView) findViewById(R.id.bac);
+        displayBAC.setText(Double.toString(BAC));
+
+    }
+
+    List<Drink> drinkList = Service.getInstance().getDrinksConsumed();
+
+    public void addButton(View v)
+    {
+        String drinkName;
+        double drinkSizeInOz;
+        double drinkProof;
+
+        java.util.Date today = new java.util.Date();
+        java.sql.Timestamp time1 = new java.sql.Timestamp(today.getTime());
+        long timeDrank = time1.getTime();
+
+        EditText name = (EditText) findViewById(R.id.drink_name);
+        drinkName = name.getText().toString();
+        try {
+            EditText proof = (EditText) findViewById(R.id.proof);
+            drinkProof = Double.parseDouble(proof.getText().toString());
+
+            EditText drinkOz = (EditText) findViewById(R.id.ozs);
+            drinkSizeInOz = Double.parseDouble(drinkOz.getText().toString());
+
+            addDrinkToList(drinkName, drinkSizeInOz, drinkProof, timeDrank);
+
+            Drunkeness getBAC = new Drunkeness();
+            getBAC.setBloodAlcohol();
+
+            double BAC = Service.getInstance().getUserIntoxLevel();
+
+            TextView displayBAC = (TextView) findViewById(R.id.bac);
+            displayBAC.setText(Double.toString(BAC));
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void addDrinkToList(String drinkName, double drinkSizeInOz, double drinkProof, long timeDrank)
+    {
+        Drink drink = new Drink(drinkName, drinkSizeInOz, drinkProof, timeDrank);
+        drinkList.add(drink);
+
+    }
+
+    public void mainMenu(View view)
+    {
+        Intent intent = new Intent(MyDrink.this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void clearBAC(View view)
+    {
+        Service.getInstance().setUserIntoxLevel(0.0);
+        double BAC = Service.getInstance().getUserIntoxLevel();
+
+        TextView displayBAC = (TextView) findViewById(R.id.bac);
+        displayBAC.setText(Double.toString(BAC));
     }
 }
